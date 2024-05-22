0a6744e1bf89efe1350eca66aa767018f07bfcd0
Ferry Febriansyah <perriebenhard@gmail.com>
2024-05-23 02:44:51 +0700

    feat: make user model and its migration

---
+++ migrations/20240522180952-create-user.js
@@ -0,0 +1,53 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable("users", {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            name: {
+                allowNull: false,
+                type: Sequelize.STRING,
+            },
+            email: {
+                allowNull: false,
+                type: Sequelize.STRING,
+            },
+            password: {
+                allowNull: false,
+                type: Sequelize.TEXT,
+            },
+            image: {
+                allowNull: false,
+                type: Sequelize.TEXT,
+            },
+            phoneNumber: {
+                allowNull: false,
+                type: Sequelize.STRING,
+            },
+            isVerified: {
+                allowNull: false,
+                type: Sequelize.BOOLEAN,
+            },
+            deletedAt: {
+                allowNull: true,
+                type: Sequelize.DATE,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable("users");
+    },
+};
