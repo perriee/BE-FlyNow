8b050b3bfe2076e31a2994f43a44cd88bc2836ee
Shahrizan <shahrizan.code@gmail.com>
2024-05-22 22:53:09 +0700

    chore: Add configuration files and models for airlines (#1)

---
+++ migrations/20240522153024-create-airline.js
@@ -0,0 +1,36 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('airline', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      airlineCode: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      airlineName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('airlines');
+  },
+};
