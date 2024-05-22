3b84caf73e2ab895144583fb9822503c20473a25
Achmad Maulana Alawi <alawitch.b@gmail.com>
2024-05-22 23:38:42 +0700

    create payment model

---
+++ models/payment.js
@@ -0,0 +1,29 @@
+"use strict"
+const { Model } = require("sequelize")
+module.exports = (sequelize, DataTypes) => {
+  class payment extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  payment.init(
+    {
+      bookingId: DataTypes.BIGINT,
+      paymentAmount: DataTypes.INTEGER,
+      paymentMethod: DataTypes.STRING,
+      paymentStatus: DataTypes.ENUM("paid", "pending", "unpaid"),
+    },
+    {
+      sequelize,
+      modelName: "payment",
+      tableName: "payment",
+      paranoid: true,
+    }
+  )
+  return payment
+}
