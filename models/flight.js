7a53aea6d726272f26804392492f1818c1363304
stefhan235 <stefhansalim12@gmail.com>
2024-05-24 19:35:18 +0700

    add relation seat

--- models/flight.js
+++ models/flight.js
@@ -1,74 +1,75 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class flight extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    class flight extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+            flight.hasMany(models.seat, { foreignKey: "flightId" });
+        }
     }
-  }
-  flight.init(
-    {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: DataTypes.INTEGER,
-      },
-      flightCode: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      terminal: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      departureAirportId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-      arrivalAirportId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-      airlineId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-      departureTime: {
-        type: DataTypes.DATE,
-        allowNull: false,
-      },
-      arrivalTime: {
-        type: DataTypes.DATE,
-        allowNull: false,
-      },
-      information: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-      },
-      deletedAt: {
-        type: DataTypes.DATE,
-        allowNull: false,
-      },
-      createdAt: {
-        type: DataTypes.DATE,
-        allowNull: false,
-      },
-      updatedAt: {
-        type: DataTypes.DATE,
-        allowNull: false,
-      },
-  }, {
-    sequelize,
-    modelName: 'flight',
-    paranoid: true,
-  });
-  return flight;
+    flight.init(
+        {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: DataTypes.INTEGER,
+            },
+            flightCode: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            terminal: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            departureAirportId: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+            },
+            arrivalAirportId: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+            },
+            airlineId: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+            },
+            departureTime: {
+                type: DataTypes.DATE,
+                allowNull: false,
+            },
+            arrivalTime: {
+                type: DataTypes.DATE,
+                allowNull: false,
+            },
+            information: {
+                type: DataTypes.TEXT,
+                allowNull: false,
+            },
+            deletedAt: {
+                type: DataTypes.DATE,
+                allowNull: true,
+            },
+            createdAt: {
+                type: DataTypes.DATE,
+                allowNull: false,
+            },
+            updatedAt: {
+                type: DataTypes.DATE,
+                allowNull: false,
+            },
+        },
+        {
+            sequelize,
+            modelName: "flight",
+            paranoid: true,
+        }
+    );
+    return flight;
 };
