a2ac1cc4e25b59506ec31ec7085394b8db86d7b2
Ferry Febriansyah <perriebenhard@gmail.com>
2024-05-24 20:11:09 +0700

    feat: add relationship user and booking

--- models/booking.js
+++ models/booking.js
@@ -1,33 +1,34 @@
 "use strict";
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class booking extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      booking.hasMany(models.passenger, { foreignKey: "bookingId" });
+    class booking extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+            booking.belongsTo(models.user, { foreignKey: "userId" });
+            booking.hasMany(models.passenger, { foreignKey: "bookingId" });
+        }
     }
-  }
-  booking.init(
-    {
-      bookingCode: DataTypes.STRING,
-      flightId: DataTypes.BIGINT,
-      userId: DataTypes.BIGINT,
-      numAdults: DataTypes.INTEGER,
-      numChildre: DataTypes.INTEGER,
-      numBabies: DataTypes.INTEGER,
-      deletedAt: DataTypes.DATE,
-    },
-    {
-      sequelize,
-      modelName: "booking",
-      paranoid: true,
-      timestamps: true,
-    }
-  );
-  return booking;
+    booking.init(
+        {
+            bookingCode: DataTypes.STRING,
+            flightId: DataTypes.BIGINT,
+            userId: DataTypes.BIGINT,
+            numAdults: DataTypes.INTEGER,
+            numChildre: DataTypes.INTEGER,
+            numBabies: DataTypes.INTEGER,
+            deletedAt: DataTypes.DATE,
+        },
+        {
+            sequelize,
+            modelName: "booking",
+            paranoid: true,
+            timestamps: true,
+        }
+    );
+    return booking;
 };
