1b791219e4083e67678e4b15e2b8ac174ccfbfaf
Ferry Febriansyah <96556755+perriee@users.noreply.github.com>
2024-05-24 20:37:45 +0700

    Add relationship in user and booking (#18)
    * feat: add relationship user and booking
    
    * fix: add unique in user email

--- models/user.js
+++ models/user.js
@@ -9,6 +9,7 @@ module.exports = (sequelize, DataTypes) => {
          */
         static associate(models) {
             // define association here
+            user.hasMany(models.booking, { foreignKey: "userId" });
         }
     }
     user.init(
