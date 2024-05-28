bf34bb415380aee9e3eb2fad4691828817c181f1
Achmad Maulana Alawi <106646206+AchmadMaulanaAlawi@users.noreply.github.com>
2024-05-28 11:31:54 +0700

    Api payment (#32)
    * create api payment route
    
    * adding getPaymentByBookingId, deletePayment, updatePayment APIs
    
    * remove 'semi' in .prettierrc
    
    * switching 'singleQuote' to false in .prettierrc

---
+++ repository/payment/index.js
@@ -0,0 +1,54 @@
+const { payment } = require('../../models')
+
+exports.getPayments = async () => {
+    const data = await payment.findAll()
+    return data
+}
+
+// exports.getPaymentById = async (id) => {
+//     const data = await payment.findAll({ where: { id } })
+//     return data
+// }
+
+exports.getPaymentByBookingId = async (bookingId) => {
+    const data = await payment.findAll({ where: { bookingId } })
+    return data
+}
+
+exports.createPayment = async (payload) => {
+    const data = await payment.create({
+        bookingId: payload.bookingId,
+        paymentAmount: payload.paymentAmount,
+        paymenMethod: payload.paymentMethod,
+        paymentStatus: payload.paymentStatus,
+    })
+    return data
+}
+
+exports.deletePayment = async (id) => {
+    const data = await payment.destroy({ where: { id } })
+    return data
+}
+
+exports.updatePayment = async (payload) => {
+    const { paymentAmount, paymentMethod, paymentStatus, bookingId } = payload
+    const currentData = await this.getPaymentByBookingId(bookingId)
+
+    if (currentData.length) {
+        const newData = await payment.update(
+            {
+                paymentAmount,
+                paymentMethod,
+                paymentStatus,
+            },
+            {
+                where: {
+                    bookingId,
+                },
+            },
+        )
+        return newData
+    } else {
+        throw new Error(`Booking with id ${bookingId} is not found`)
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,12 @@ const auth = require("./auth");
 const passenger = require("./passenger");
 const airline = require("./airline");
 const airport = require("./airport");
+const payment = require("./payment");
 
 router.use("/auth", auth);
 router.use("/passengers", passenger);
 router.use("/airlines", airline);
 router.use("/airports", airport);
+router.use("/payment", payment);
 
 module.exports = router;
