5e065be9ec765a1eb3c75eb7262ff5c79de8ade0
hikmal_a.s <93532269+MituBaby@users.noreply.github.com>
2024-05-28 11:56:17 +0700

    create api crud in flight (#31)
    * create api crud in flight
    
    * add include data from model airport & airline

---
+++ repository/flight/index.js
@@ -0,0 +1,76 @@
+const { where } = require("sequelize");
+const { flight, airport, airline, seat, booking } = require("../../models");
+
+exports.getAllFlights = async () => {
+    const data = await flight.findAll({
+        include: [
+            {
+                model: airport,
+            },
+            {
+                model: airline,
+            },
+        ],
+    });
+    return data;
+};
+
+exports.getFlight = async (id) => {
+    const data = await flight.findAll({
+        where: {
+          id,
+        },
+        include: [
+            {
+                model: airport,
+            },
+            {
+                model: airline,
+            },
+        ],
+    });
+
+    if (data.length > 0) {
+        return data[0];
+    }
+    
+    throw new Error(`Flights is not found!`);
+};
+
+exports.createFlight = async (payload) => {
+    const data = await flight.create(payload);
+    return data;
+};
+
+exports.updateFlight = async (id, payload) => {
+    await flight.update(payload, {
+        where: {
+            id,
+        },
+    });
+
+    const data = await flight.findAll({
+        where: {
+            id,
+        },
+        include: [
+            {
+                model: airport,
+            },
+            {
+                model: airline,
+            },
+        ],
+    });
+    
+    if (data.length > 0) {
+        return data[0];
+    }
+
+    throw new Error(`Flights is not found!`);
+};
+
+exports.deleteFlight = async (id) => {
+    await flight.destroy({ where: { id }});
+    return null;
+}
--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,13 @@ const passenger = require("./passenger");
 const airline = require("./airline");
 const airport = require("./airport");
 const payment = require("./payment");
+const flight = require("./flight");
 
 router.use("/auth", auth);
 router.use("/passengers", passenger);
 router.use("/airlines", airline);
 router.use("/airports", airport);
 router.use("/payment", payment);
+router.use("/flight", flight);
 
 module.exports = router;
