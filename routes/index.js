c5d1352942b768c385bb50036282c42e1dc35224
Shahrizan <shahrizan.code@gmail.com>
2024-05-28 19:54:14 +0700

    add auth middleware for airline endpoint

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,10 @@ const router = express.Router();
 const auth = require("./auth");
 const passenger = require("./passenger");
 const airline = require("./airline");
+const { authMiddleware } = require("../middleware/auth");
 
 router.use("/auth", auth);
 router.use("/passengers", passenger);
-router.use("/airlines", airline);
+router.use("/airlines", authMiddleware(), airline);
 
 module.exports = router;
