5c4659d8a5d5598092624a22ab731b57827caa39
Shahrizan <shahrizan.code@gmail.com>
2024-06-02 11:02:36 +0700

    refactor: update booking API to use departureFlightId and returnFlightId

--- controller/booking.js
+++ controller/booking.js
@@ -38,65 +38,69 @@ exports.createBooking = async (req, res, next) => {
     try {
         const {
             bookingCode,
-            flightId,
+            departureFlightId,
+            returnFlightId = null,
             userId,
             numAdults,
             numChildren,
             numBabies,
         } = req.body;
 
-        if (!bookingCode || bookingCode == "") {
+        if (!bookingCode || bookingCode === "") {
             return next({
                 message: "Booking Code must be filled",
                 statusCode: 400,
             });
         }
-        if (!flightId || flightId == "") {
+        if (!departureFlightId || departureFlightId === "") {
             return next({
-                message: "Flight Id must be filled",
+                message: "Departure Flight Id must be filled",
                 statusCode: 400,
             });
         }
-        if (!userId || userId == "") {
+        if (!userId || userId === "") {
             return next({
                 message: "User Id must be filled",
                 statusCode: 400,
             });
         }
-        if (!numAdults || numAdults == "") {
+        if (!numAdults || numAdults === "") {
             return next({
                 message: "Number Adults must be filled",
                 statusCode: 400,
             });
         }
-        //   if (!numChildren || numChildren == "") {
-        //       return next({
-        //           message: "Number Children must be filled",
-        //           statusCode: 400,
-        //       });
-        //   }
-        //   if (!numBabies || numBabies == "") {
-        //       return next({
-        //           message: "Number Babies must be filled",
-        //           statusCode: 400,
-        //       });
-        //   }
+
+        if (numChildren && typeof numChildren !== "number") {
+            return next({
+                message: "Number Children must be number",
+                statusCode: 400,
+            });
+        }
+
+        if (numBabies && typeof numBabies !== "number") {
+            return next({
+                message: "Number Babies must be filled",
+                statusCode: 400,
+            });
+        }
 
         const data = await bookingUsecase.createBooking({
             bookingCode,
-            flightId,
+            departureFlightId,
+            returnFlightId,
             userId,
             numAdults,
             numChildren,
             numBabies,
         });
 
-        res.status(200).json({
+        return res.status(200).json({
             message: "Success",
             data,
         });
     } catch (error) {
-        next(error);
+        return next(error);
     }
 };
 
