9e0d38ebaed8a59652e339aa09afb597a39aa855
Randha Yoga <92174528+randhayoga@users.noreply.github.com>
2024-06-24 13:13:29 +0700

    feat: usecases for notification

---
+++ usecase/notification/index.js
@@ -0,0 +1,53 @@
+const {
+    createNotification,
+    getAllNotifications,
+    getNotificationByID,
+    getNotificationsByUserID,
+    updateNotification,
+    deleteNotification,
+    searchNotification,
+} = require("../../repository/notification");
+
+exports.createNotification = async (payload) => {
+    return await createNotification(payload);
+};
+
+exports.getAllNotifications = async () => {
+    return await getAllNotifications();
+};
+
+exports.getNotificationByID = async (id) => {
+    return await getNotificationByID(id);
+};
+
+exports.getAllNotificationsByUserID = async (userId) => {
+    return await getNotificationsByUserID(userId);
+};
+
+exports.getPaymentNotificationsByUserID = async (userId) => {
+    return await getNotificationsByUserID(userId, { type: "payment" });
+};
+
+exports.getFlightNotificationsByUserID = async (userId) => {
+    return await getNotificationsByUserID(userId, { type: "flight" });
+};
+
+exports.getPromoNotificationsByUserID = async (userId) => {
+    return await getNotificationsByUserID(userId, { type: "promo" });
+};
+
+exports.updateNotification = async (id, payload) => {
+    return await updateNotification(id, payload);
+};
+
+exports.readNotification = async (id) => {
+    return await updateNotification(id, { isRead: true });
+};
+
+exports.deleteNotification = async (id) => {
+    return await deleteNotification(id);
+};
+
+exports.searchNotification = async (payload) => {
+    return await searchNotification(payload);
+};
