0acc04e0569229687b1d30cce023623cc4c63a18
Ran <prandhayoga@gmail.com>
2024-06-27 09:56:19 +0700

    feat: check unread notification api (#77)

--- routes/notification.js
+++ routes/notification.js
@@ -14,6 +14,7 @@ const {
     readNotification,
     deleteNotification,
     searchNotification,
+    checkUnreadNotifications,
 } = require("../controller/notification");
 
 /* For Users (will be directly called in FE) */
@@ -24,6 +25,7 @@ router.get("/promo", getPromoNotificationsByUserID);
 router.get("/booking", getBookingNotificationsByUserID);
 router.patch("/read/:id", readNotification);
 router.get("/search", searchNotification);
+router.get("/checkUnread", checkUnreadNotifications);
 
 /* For "Admin" (won't be directly called in FE) */
 router.route("/").post(createNotification).get(getAllNotifications);
